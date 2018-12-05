d901cadcb0cb6c1bb6ce663772499ac186114772
adminvasheypomoiki <kek@cheburek.lol>
2018-12-05 22:23:32 +0300

    normed

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,16 +6,15 @@
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 21:20:08 by qhetting          #+#    #+#             */
-/*   Updated: 2018/12/05 21:20:11 by qhetting         ###   ########.fr       */
+/*   Updated: 2018/12/05 22:11:49 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
 	free(*alst);
 	*alst = NULL;
 }
-
--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,33 +6,33 @@
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 21:21:01 by qhetting          #+#    #+#             */
-/*   Updated: 2018/12/05 21:21:02 by qhetting         ###   ########.fr       */
+/*   Updated: 2018/12/05 22:13:05 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list *getlist()
+static t_list		*getlist(void)
 {
 	t_list *chain;
-	chain = (t_list *) malloc(sizeof(t_list));
-	return chain;
+
+	chain = (t_list *)malloc(sizeof(t_list));
+	return (chain);
 }
 
-t_list *ft_lstnew(void const *content, size_t content_size)
+t_list				*ft_lstnew(void const *content, size_t content_size)
 {
-
 	t_list *chain;
 
 	chain = getlist();
-	if(!chain)
+	if (!chain)
 		return (NULL);
 	if (!content)
 	{
-			chain->next = NULL;
-			chain->content = NULL;
-			chain->content_size = 0;
-			return (chain);
+		chain->next = NULL;
+		chain->content = NULL;
+		chain->content_size = 0;
+		return (chain);
 	}
 	chain->content = ft_memalloc(content_size);
 	if (!chain->content)
@@ -40,7 +40,7 @@ t_list *ft_lstnew(void const *content, size_t content_size)
 		free(chain);
 		return (NULL);
 	}
-	ft_memcpy((void *)chain->content,(const void *) content, content_size);
+	ft_memcpy(chain->content, (const void *)content, content_size);
 	chain->content_size = content_size;
 	chain->next = NULL;
 	return (chain);
