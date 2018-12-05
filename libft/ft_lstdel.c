00503cf62496b2e0e75af5c3121a88f370b26444
adminvasheypomoiki <kek@cheburek.lol>
2018-12-05 21:57:22 +0300

    ft cheat

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -1,19 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/05 21:19:51 by qhetting          #+#    #+#             */
-/*   Updated: 2018/12/05 21:21:28 by qhetting         ###   ########.fr       */
+/*   Created: 2018/12/05 21:19:57 by qhetting          #+#    #+#             */
+/*   Updated: 2018/12/05 21:19:58 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	new->next = *alst;
-	*alst = new;
+	t_list *list;
+	t_list *next;
+
+	list = *alst;
+	if (!list)
+		return ;
+	while (list)
+	{
+		next = list;
+		ft_lstdelone(&list, del);
+		list = next->next;
+	}
+	*alst = NULL;
 }
